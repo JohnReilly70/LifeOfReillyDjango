4a927d6d47e19bd5b0431368061b60c7cf2b649b
SgtSafety <johnreilly1989@hotmail.co.uk>
2016-12-07 20:42:31 +0000

    Added Blog & Secret Page/ created base template and updated html to match

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
+    url(r'blog.html', views.blog, name='post_list'),
+    url(r'Secret-Area.html', views.Secret_Area)
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
-
+from django.utils import timezone
+from .models import Post
 # Create your views here.
 
 def post_list(request):
     return render(request, 'blog/post_list.html', {})
+
+def blog(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/blog.html', {'posts': posts})
+
+def Secret_Area(request):
+    return render(request, 'blog/Secret-Area.html', {})
